0ce84d8b6f870cf4f47a48617bbf49c46a55a8ac
Roman Romanko <emad.frontend.wrk@gmail.com>
2024-10-18 01:51:00 +0300

    Added Onboarding Screen

---
+++ src/processes/auth/screens/OnboardingScreen/components/index.tsx
@@ -0,0 +1,85 @@
+import { SafeAreaView } from 'moti'
+import { FlatList, Text, TouchableOpacity, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { windowWidth } from 'shared/types'
+import { theme } from 'theme'
+
+export const itemFix = { itemVisiblePercentThreshold: 150 }
+
+export const Dot = ({ color, size }) => {
+  return (
+    <View
+      style={{
+        width: size,
+        marginHorizontal: 6,
+        height: size,
+        borderRadius: 50,
+        backgroundColor: color,
+      }}
+    />
+  )
+}
+export const Dots = ({ pages, currentPage }) => {
+  const dotList = Array(pages.length).fill({ size: 14, color: '#d5d5d5' })
+  return (
+    <FlatList
+      style={{ flexGrow: 0 }}
+      data={dotList}
+      renderItem={({ item, index }) => (
+        <Dot size={item.size} color={index === currentPage ? theme.green : item.color} />
+      )}
+      horizontal
+    />
+  )
+}
+type paginationPropsType = {
+  leftButtonText?: string
+  rightButtonText?: string
+  rightButtonIcon?: string
+  onRightClick?: () => void
+  onLeftClick?: () => void
+  pages: any[]
+  currentPage: number
+  buttonsOff?: boolean
+}
+export const Pagination = ({
+  leftButtonText,
+  rightButtonText,
+  rightButtonIcon,
+  onRightClick,
+  onLeftClick,
+  pages,
+  currentPage,
+  buttonsOff = false,
+}: paginationPropsType) => {
+  return (
+    <SafeAreaView
+      style={{
+        width: windowWidth,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingVertical: 12,
+        height: 64,
+      }}
+    >
+      {!buttonsOff && (
+        <TouchableOpacity
+          style={{ maxWidth: windowWidth * 0.35, minWidth: 124, alignItems: 'center' }}
+          onPress={onLeftClick}
+        >
+          <Text>{leftButtonText}</Text>
+        </TouchableOpacity>
+      )}
+      <Dots pages={pages} currentPage={currentPage} />
+      {!buttonsOff && (
+        <TouchableOpacity
+          style={{ maxWidth: windowWidth * 0.35, minWidth: 124, alignItems: 'center' }}
+          onPress={onRightClick}
+        >
+          <Text>{rightButtonIcon ? <Icon name={rightButtonIcon} size={32} color={'#7d7'} /> : rightButtonText}</Text>
+        </TouchableOpacity>
+      )}
+    </SafeAreaView>
+  )
+}
---
+++ src/processes/auth/screens/OnboardingScreen/index.tsx
@@ -0,0 +1,102 @@
+import { View, Image, FlatList, Text } from 'react-native'
+import { SafeAreaView } from 'moti'
+import { useCallback, useRef, useState } from 'react'
+import { gatewaySetUserOnboarded } from 'shared/lib/api'
+import { useAppDispatch, userUpdateOnboarding } from 'shared/store'
+import { itemFix, Pagination } from 'src/processes/auth/screens/OnboardingScreen/components'
+import { windowWidth } from 'shared/types'
+
+import styles from '../styles'
+
+const pages = [
+  {
+    header: 'Довіра',
+    subHeader:
+      'Ми будуємо прозорі та надійні інвестиційні рішення для вашої впевненості. Ваші кошти в надійних руках з Ecobank Investment.',
+    image: 'https://i.ibb.co/jVNYVZL/Removebg-Preview-Oct-18.png',
+  },
+  {
+    header: 'Захист',
+    subHeader:
+      'Безпека ваших фінансів та даних — наш головний пріоритет. Використовуємо сучасні технології для максимального захисту на кожному етапі.',
+    image: 'https://i.ibb.co/K2JMNfT/Removebg-preview.png',
+  },
+  {
+    header: 'Майбутнє',
+    subHeader:
+      'Відкрийте нові можливості для пасивного доходу. Розвивайтесь та навчайтесь з Дія Освіта, інвестуючи у своє майбутнє.',
+    image: 'https://i.ibb.co/ysQWgPD/Remove-BG-Preview-1.png',
+  },
+]
+
+export default function OnboardingScreen() {
+  const dispatch = useAppDispatch()
+
+  const [currentPage, setCurrentPage] = useState(0)
+  const flatList = useRef<FlatList>(null)
+
+  const setOnboardedUser = () => {
+    gatewaySetUserOnboarded()
+    dispatch(userUpdateOnboarding())
+  }
+  const onNextPage = () => {
+    flatList.current?.scrollToIndex({
+      animated: true,
+      index: currentPage + 1,
+    })
+  }
+  const onPreviousPage = () => {
+    flatList.current?.scrollToIndex({
+      animated: true,
+      index: currentPage - 1,
+    })
+  }
+  const onSwipe = ({ viewableItems }) => {
+    if (!viewableItems[0]) return
+    setCurrentPage((prevState) => {
+      if (prevState !== viewableItems[0]) return viewableItems[0].index
+    })
+  }
+
+  const renderItem = useCallback(({ item }) => {
+    return (
+      <View style={{ flex: 1, width: windowWidth, justifyContent: 'space-around' }}>
+        <View style={{ alignSelf: 'center', alignItems: 'center', gap: 12 }}>
+          <View style={{ borderRadius: 35 }}>
+            <Image source={{ uri: item.image }} style={{ width: windowWidth, aspectRatio: 1 }} />
+          </View>
+          <Text style={[styles.title, { paddingHorizontal: 24 }]}>{item.header}</Text>
+          <Text style={[styles.description, { paddingHorizontal: 24 }]}>{item.subHeader}</Text>
+        </View>
+      </View>
+    )
+  }, [])
+
+  const onSwipeRef = useRef(onSwipe)
+  return (
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <FlatList
+          ref={flatList}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          onViewableItemsChanged={onSwipeRef.current}
+          initialNumToRender={1}
+          viewabilityConfig={itemFix}
+          data={pages}
+          renderItem={renderItem}
+        />
+        <Pagination
+          leftButtonText={currentPage ? 'Назад' : 'Пропустити'}
+          rightButtonText={'Далі'}
+          rightButtonIcon={currentPage === pages.length - 1 && 'check'}
+          onLeftClick={!currentPage ? setOnboardedUser : onPreviousPage}
+          onRightClick={currentPage === pages.length - 1 ? setOnboardedUser : onNextPage}
+          currentPage={currentPage}
+          pages={pages}
+        />
+      </SafeAreaView>
+    </View>
+  )
+}
